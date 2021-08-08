90414cfcab1863762dca8f00595e18e11ad340bc
Bradley Reynolds <bradley.reynolds@darbia.dev>
2021-08-07 20:58:04 -0500

    Add support for unbound actions

--- darbiadev_businesscentral/businesscentral_services.py
+++ darbiadev_businesscentral/businesscentral_services.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
-import json
 from typing import Union
-from urllib.parse import quote
 
 import requests
 
@@ -22,14 +20,14 @@ class BusinessCentralServices:
             web_service_access_key: str
     ):
         self.base_url: str = base_url
-        self.tenant_id: str = tenant_id
+        self.__tenant_id: str = tenant_id
         self.environment: str = environment
         self.company_name: str = company_name
-        self.username: str = username
-        self.web_service_access_key: str = web_service_access_key
+        self.__username: str = username
+        self.__web_service_access_key: str = web_service_access_key
 
-        self.odata_base_url: str = base_url + f'{self.tenant_id}/{self.environment}/ODataV4/'
-        self.odata_company: str = f"Company('{quote(company_name)}')/"
+        self.odata_base_url: str = base_url + f'{self.__tenant_id}/{self.environment}/ODataV4/'
+        self.odata_company: str = f"Company('{company_name}')/"
         self.odata_url: str = self.odata_base_url + self.odata_company
 
     def _build_resource_url(
@@ -54,10 +52,17 @@ class BusinessCentralServices:
             resource_url = resource_url[:-1] + ')'
         return self.odata_url + resource_url
 
+    def _build_unbound_action_url(
+            self,
+            codeunit: str,
+            procedure: str
+    ):
+        return self.odata_base_url + f'{codeunit}_{procedure}/'
+
     def _make_request(
             self,
             method: str,
-            resource: str = None,
+            resource: str,
             resource_data: dict[str, Union[str, int, bool]] = None,
             values: list[str] = None,
             params: dict[str, str] = None,
@@ -66,18 +71,37 @@ class BusinessCentralServices:
         args = {
             'method': method,
             'url': self._build_resource_url(resource=resource, values=values),
-            'auth': (self.username, self.web_service_access_key),
+            'auth': (self.__username, self.__web_service_access_key),
             'headers': {'Content-Type': 'application/json'}
         }
 
         if resource_data is not None:
-            args['data'] = json.dumps(resource_data)
+            args['json'] = resource_data
 
         if etag is not None:
             args['headers']['If-Match'] = etag
 
         if params is not None:
             args['params'] = params
+
+        response = requests.request(**args)
+        response.raise_for_status()
+        return response.json()
+
+    def _make_unbound_request(
+            self,
+            codeunit: str,
+            procedure: str,
+            data: dict[str, str]
+    ) -> dict:
+        args = {
+            'method': 'POST',
+            'url': self._build_unbound_action_url(codeunit=codeunit, procedure=procedure),
+            'auth': (self.__username, self.__web_service_access_key),
+            'headers': {'Content-Type': 'application/json'},
+            'params': {'company': self.company_name},
+            'json': data
+        }
 
         response = requests.request(**args)
         response.raise_for_status()
